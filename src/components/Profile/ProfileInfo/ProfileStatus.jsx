226d612371bf89eb5d3fe08c5bcbb1f4602f04d1
hampachovsky <hampe1324@gmail.com>
2021-10-16 22:02:02 +0300

    Rewrited Profile status component to hook.

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,53 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import style from './ProfileInfo.module.css';
 
-class ProfileStatus extends React.Component {
-  state = {
-    editMode: false,
-    status: this.props.status,
-  };
+function ProfileStatus(props) {
+  const [editMode, setEditMode] = useState(false);
+  const [status, setStatus] = useState(props.status);
 
-  toggleEditMode = () => {
-    this.setState({ editMode: !this.state.editMode });
-  };
-  onEnd = () => {
-    this.setState({ editMode: false });
-    this.props.updateUserStatus(this.state.status);
-  };
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
-  onStatusChange = (e) => {
-    this.setState({ status: e.target.value });
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.status !== this.props.status) {
-      this.setState({ status: this.props.status });
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.editMode === false ? (
-          <div>
-            <p onDoubleClick={this.toggleEditMode} className={style.aboutMe}>
-              {this.props.status ? this.props.status : 'No status'}
-            </p>
-          </div>
-        ) : (
-          <div className={style.profileStatusInputContainer}>
-            <input
-              autoFocus={true}
-              onBlur={this.onEnd}
-              onChange={this.onStatusChange}
-              className={style.profileStatusInput}
-              value={this.state.status}
-            />
-          </div>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {!editMode ? (
+        <div>
+          <p onDoubleClick={() => setEditMode(true)} className={style.aboutMe}>
+            {props.status ? props.status : 'No status'}
+          </p>
+        </div>
+      ) : (
+        <div className={style.profileStatusInputContainer}>
+          <input
+            className={style.profileStatusInput}
+            autoFocus={true}
+            onChange={(e) => setStatus(e.target.value)}
+            onBlur={() => {
+              setEditMode(false);
+              props.updateUserStatus(status);
+            }}
+            value={status}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default ProfileStatus;
---
+++ src/z OldFunc/ProfileStatus.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import style from './ProfileInfo.module.css';
+
+class ProfileStatus extends React.Component {
+  state = {
+    editMode: false,
+    status: this.props.status,
+  };
+
+  toggleEditMode = () => {
+    this.setState({ editMode: !this.state.editMode });
+  };
+  onEnd = () => {
+    this.setState({ editMode: false });
+    this.props.updateUserStatus(this.state.status);
+  };
+
+  onStatusChange = (e) => {
+    this.setState({ status: e.target.value });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({ status: this.props.status });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.editMode === false ? (
+          <div>
+            <p onDoubleClick={this.toggleEditMode} className={style.aboutMe}>
+              {this.props.status ? this.props.status : 'No status'}
+            </p>
+          </div>
+        ) : (
+          <div className={style.profileStatusInputContainer}>
+            <input
+              autoFocus={true}
+              onBlur={this.onEnd}
+              onChange={this.onStatusChange}
+              className={style.profileStatusInput}
+              value={this.state.status}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default ProfileStatus;
